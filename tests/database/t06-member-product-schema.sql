ddaf3a638073f9e78bd413da8f8d14b5683b8cd0
qtaro-dev <qtaro.dev.project1977@gmail.com>
2026-09-25 05:15:13 +0900

    test(db): cover member schema foreign key

--- tests/database/t06-member-product-schema.sql
+++ tests/database/t06-member-product-schema.sql
@@ -21,6 +21,13 @@ do $$ begin
     raise exception 'expected negative product price to fail';
   exception when check_violation then null;
   end;
+
+  begin
+    insert into public.addresses(user_id,recipient_name,postal_code,prefecture_code,city,street)
+    values ('00000000-0000-0000-0000-000000000099','No User','1000001',13,'千代田区','千代田1-1');
+    raise exception 'expected address owner foreign key to fail';
+  exception when foreign_key_violation then null;
+  end;
 end $$;
 
 -- Incomplete drafts are valid and can omit shipping data.
